c07e37cef29a55326562ccc20b6172f5cdc69f42
EmiRoberti77 <emiliano.roberti@accenture.com>
2023-12-19 10:44:24 +0000

    added promptTemplate for responses

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import dotenv from 'dotenv';
 import { Prompt } from './prompts/prompt.js';
+import { PrompTemplateClass } from './prompts/prompTemplate.js';
 dotenv.config();
 
 const openaiApiKey = process.env.OPENAI_API_KEY;
@@ -7,3 +8,5 @@ const openaiApiKey = process.env.OPENAI_API_KEY;
 const p = new Prompt(openaiApiKey);
 await p.run();
 //run template with a prompt
+const pt = new PrompTemplateClass(openaiApiKey);
+await pt.run();
--- src/prompts/prompTemplate.js
+++ src/prompts/prompTemplate.js
@@ -1,8 +1,26 @@
-import { PrompTemplate } from 'langchain/prompts';
-import { Base } from './base';
+import { PromptTemplate } from 'langchain/prompts';
+import { ChatOpenAI } from 'langchain/chat_models/openai';
+import { Base } from './base.js';
 
-export class PrompTemplate extends Base {
+export class PrompTemplateClass extends Base {
+  prompt;
   constructor(openAIApiKey) {
     super(openAIApiKey);
+
+    this.prompt = PromptTemplate.fromTemplate(
+      'what is a good name for a company that names {product}?'
+    );
+  }
+
+  async run() {
+    const formattedPrompt = await this.prompt.format({
+      product: 'socks',
+    });
+
+    console.log(formattedPrompt);
+
+    const llm = new ChatOpenAI();
+    const llmResponse = await llm.predict(formattedPrompt);
+    console.log('llm promptTemplate', llmResponse);
   }
 }
